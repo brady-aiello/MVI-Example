21e13fa336f0b5fd499ec6b84e61b13cedb15dc7
Brady Aiello <brady.aiello@gmail.com>
2020-05-31 19:08:29 -0700

    Lesson 5: StateEvents https://codingwithmitch.com/courses/model-view-intent-mvi-architecture/stateevents/
    Added MainStateEvent sealed class to wrap our events for
    	1) get blog post
    	2) get user
    	3) none
    
    For none events and placeholder transformations, we stole AbsentLiveData from Android architecture component samples:
    https://github.com/android/architecture-components-samples/blob/master/GithubBrowserSample/app/src/main/java/com/android/example/github/util/AbsentLiveData.kt

--- app/src/main/java/com/codingwithmitch/mviexample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/codingwithmitch/mviexample/ui/main/MainViewModel.kt
@@ -2,11 +2,36 @@ package com.codingwithmitch.mviexample.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.codingwithmitch.mviexample.ui.main.state.MainStateEvent
+import com.codingwithmitch.mviexample.ui.main.state.MainStateEvent.*
 import com.codingwithmitch.mviexample.ui.main.state.MainViewState
+import com.codingwithmitch.mviexample.util.AbsentLiveData
 
 class MainViewModel: ViewModel() {
+    private val _stateEvent: MutableLiveData<MainStateEvent> = MutableLiveData(None)
     private val _viewState: MutableLiveData<MainViewState> = MutableLiveData()
+
     val viewState: LiveData<MainViewState>
         get() = _viewState
+    val dataState: LiveData<MainViewState> = Transformations
+        .switchMap(_stateEvent) { stateEvent ->
+        handleStateEvent(stateEvent)
+    }
+
+    private fun handleStateEvent(stateEvent: MainStateEvent): LiveData<MainViewState> {
+        return when (stateEvent) {
+            is GetBlogPostsEvent -> {
+                AbsentLiveData.create()
+            }
+            is GetUserEvent -> {
+                AbsentLiveData.create()
+            }
+            is None -> {
+                AbsentLiveData.create()
+            }
+        }
+    }
+
 }
