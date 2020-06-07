83d2c7f748a63ce189545717dd8cbf2667bf9a30
Brady Aiello <brady.aiello@gmail.com>
2020-06-07 10:11:19 -0700

    Lesson 7: Kotlin Singletons and Retrofit https://codingwithmitch.com/courses/model-view-intent-mvi-architecture/kotlin-singletons-and-retrofit/
    Made our ApiService for Mitch's OpenApi server. Made our RetrofitBuilder object which holds references to a lazy Retrofit.Builder and a lazy ApiService. If we  were making a real app, we might use Dagger to make this a Singleton, and maybe scope it as well. But with things as simple as they are, a simple Kotlin object notation works just fine.

---
+++ app/src/main/java/com/codingwithmitch/mviexample/api/ApiService.kt
@@ -0,0 +1,14 @@
+package com.codingwithmitch.mviexample.api
+
+import com.codingwithmitch.mviexample.model.BlogPost
+import com.codingwithmitch.mviexample.model.User
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("placeholder/blogs")
+    fun getUser(): List<BlogPost>
+
+    @GET("placeholder/user/{userId}")
+    fun getUser(@Path("userId") userId: String): User
+}
